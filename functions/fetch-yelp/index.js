4e0edcd75a8a5731c17be0e28705cf378f8eaa35
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-02-06 15:58:45 -0800

    fetching businesses and searching based on zip

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -2,8 +2,32 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
-  // add code here to fetch data from yelp API
-  // be sure to include the parameters from event.queryStringParameters
+  const zip = event.queryStringParameters.zip;
+  const search = event.queryStringParameters.search;
+
+  try {
+    const resp = await fetch(
+      `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${search}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+        },
+      }
+    );
+
+    const data = await resp.json();
+    const json = JSON.stringify(data.businesses);
+    return {
+      statusCode: 200,
+      body: json,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
 };
 
 module.exports = { handler };
